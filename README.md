5e7dd5d15d6dad399acfe0aea869968728e1034b
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2018-03-22 00:40:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # elements-kinds
-JS helper to find the elements kinds in v8 
+
+JS helper function to check the elements kinds in v8
+
+More details in this [blogpost](https://v8project.blogspot.de/2017/09/elements-kinds-in-v8.html) by Mathias Bynes
---
+++ index.js
@@ -0,0 +1,66 @@
+const ELEMENTS_KINDS = {
+  HOLEY_SMI_ELEMENTS: "HOLEY_SMI_ELEMENTS",
+  HOLEY_DOUBLE_ELEMENTS: "HOLEY_DOUBLE_ELEMENTS",
+  HOLEY_ELEMENTS: "HOLEY_ELEMENTS",
+  PACKED_SMI_ELEMENTS: "PACKED_SMI_ELEMENTS",
+  PACKED_DOUBLE_ELEMENTS: "PACKED_DOUBLE_ELEMENTS",
+  PACKED_ELEMENTS: "PACKED_ELEMENTS"
+};
+
+function isSMI(arr) {
+  return arr.every(e => Number.isSafeInteger(e) && !Object.is(e, -0));
+}
+
+/**
+ * Handles +-Infinity, Doubles, NaN, -0
+ */
+function isDouble(arr) {
+  return arr.every(e => typeof e === "number");
+  //return arr.some(
+  //   e =>
+  //     Number.isNaN(e) || (Number(e) === e && e % 1 !== 0) || Object.is(e, -0)
+  // );
+}
+
+function containsHoles(arr) {
+  for (const ele of arr) {
+    if (typeof ele === "undefined") {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = function getElementsKind(inputArray) {
+  if (!Array.isArray(inputArray)) {
+    throw new Erorr("Input param must be array");
+  }
+
+  let kind;
+  /**
+   * Check if array contains holes
+   */
+  const isHoley = containsHoles(inputArray);
+
+  if (isHoley) {
+    if (isSMI(inputArray)) {
+      kind = ELEMENTS_KINDS.HOLEY_SMI_ELEMENTS;
+    } else if (isDouble(inputArray)) {
+      kind = ELEMENTS_KINDS.HOLEY_DOUBLE_ELEMENTS;
+    } else {
+      kind = ELEMENTS_KINDS.HOLEY_ELEMENTS;
+    }
+  } else {
+    if (isSMI(inputArray)) {
+      kind = ELEMENTS_KINDS.PACKED_SMI_ELEMENTS;
+    } else if (isDouble(inputArray)) {
+      kind = ELEMENTS_KINDS.PACKED_DOUBLE_ELEMENTS;
+    } else {
+      kind = ELEMENTS_KINDS.PACKED_ELEMENTS;
+    }
+  }
+  return kind;
+};
+
+let a = new Array(21);
+console.log(getElementsKind(a));
